0f08be118cd110e87331b21520276dbb5df6342f
Zack <zack.hixon@hotmail.com>
2014-04-02 16:51:32 -0400

    Create nothing.rs

---
+++ nothing.rs
@@ -0,0 +1,23 @@
+//Copyleft 2014 Zack Hixon
+//nothing.rs - does absolutely nothing.
+
+fn main() {
+	let mut nothing = 0;
+	let mut zilch = false;
+	if check_if_nothing(nothing) {
+		zilch = true;
+	}
+	while zilch || nothing <= 10 {
+		nothing += 1;
+		if nothing == 10 {
+			zilch = false;
+		}
+	}
+}
+
+fn check_if_nothing(nada: int) -> bool {
+	if nada == 0 {
+		return true;
+	}
+	else { return false; }
+}
